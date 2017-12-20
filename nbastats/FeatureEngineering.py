924c1b7a507d8e3d68daadd4d3c1efdc61e58260
justinbt21 <justinbt21@gmail.com>
2017-12-19 19:05:06 -0800

    Updated Trade evaluator
    added last x days performance evaluation

--- nbastats/FeatureEngineering.py
+++ nbastats/FeatureEngineering.py
@@ -13,31 +13,67 @@ from nba_py import player
 from nba_py import game
 from webscrape import getData
 from time import sleep
+from datetime import datetime
+from dateutil.relativedelta import relativedelta as tDelta
 
 # Params
-p_year = '2017'
-FantasyTeam = ['john wall', 'rudy gobert', 'lonzo ball', 'khris middleton', 'jj redick', 'aaron gordon', 'andre drummond', 'darren collison', 'will barton', 'markieff morris', 'joe ingles', 'josh richardson', 'dewayne dedmon']
+current_year = int(datetime.date(datetime.now()).strftime('%Y'))-1 if datetime.date(datetime.now()).month < 11 else int(datetime.date(datetime.now()).strftime('%Y'))
+fantasy_team = ['john wall', 'rudy gobert', 'lonzo ball', 'khris middleton', 'jj redick', 'aaron gordon', 'andre drummond', 'darren collison', 'will barton', 'markieff morris', 'joe ingles', 'josh richardson', 'dewayne dedmon']
+trade_for = ['kyle lowry', 'thaddeus young']
+trading = ['john wall', 'jj redick']
 
-playerInfo = pd.DataFrame(player.PlayerList(season='2017-18', only_current=1).info())
-"""
-def tradeEvaluator(current_team, proposed_players, receiving players, season=2017):
+playerInfo = pd.DataFrame(player.PlayerList(season=str(current_year)+'-'+str(int(current_year)+1)[-2:], only_current=1).info())
+
+def getStats(player_list, season=current_year, last_game_range = None):
     df = pd.DataFrame()
-    for i in current_team:
+    for i in player_list:
+        print(i.title())
         data = getData(i, 'GameLog', season)
+        if last_game_range:
+            data = data.sort_values('GAME_DATE')
+            data = data.loc[data.GAME_DATE >= datetime.date(datetime.now()) - tDelta(days=+last_game_range)]
+            data['PLAYER_NAME'] = i.title()
+            df = df.append(data)
+            sleep(randint(1,3))
+            continue
+        
         data['PLAYER_NAME'] = i.title()
         df = df.append(data)
-        sleep(3)
+        sleep(randint(1,3))
+
     return df
 
-"""
 
+def tradeEvaluator(current_team, proposed_players=[], received_players=[], season=current_year, last_game_range = None):
+    total_players = fantasy_team + received_players
+
+    df = getStats(total_players, season, last_game_range = last_game_range)
+
+    current_team = df.loc[df['PLAYER_NAME'].str.lower().isin(fantasy_team)]
+    new_team = df.loc[~df['PLAYER_NAME'].str.lower().isin(proposed_players)]
+
+    current_stats = current_team.groupby('PLAYER_NAME').mean()
+    current_total_stats = current_stats.sum()
+    current_total_stats['FG_PCT'] = current_total_stats['FGM']/current_total_stats['FGA']
+    current_total_stats['FG3_PCT'] = current_total_stats['FG3M']/current_total_stats['FG3A']
+    current_total_stats['FT_PCT'] = current_total_stats['FTM']/current_total_stats['FTA']
     
-# CurrentStats = pd.merge(CurrentStats, AllCurrentPlayers[['PERSON_ID','DISPLAY_FIRST_LAST']], how='left', left_on = 'PLAYER_ID', right_on = 'PERSON_ID')
+    new_stats = new_team.groupby('PLAYER_NAME').mean()
+    new_total_stats = new_stats.sum()
+    new_total_stats['FG_PCT'] = new_total_stats['FGM']/new_total_stats['FGA']
+    new_total_stats['FG3_PCT'] = new_total_stats['FG3M']/new_total_stats['FG3A']
+    new_total_stats['FT_PCT'] = new_total_stats['FTM']/new_total_stats['FTA']
 
-def combineTeam(name, season):
+    trade_eval_stats = new_total_stats - current_total_stats
+    trade_eval_stats = trade_eval_stats.drop(['VIDEO_AVAILABLE', 'Player_ID', 'PLUS_MINUS', 'MIN', 'PF', 'FGM', 'FGA', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'OREB', 'DREB'])
+
+    return trade_eval_stats.round(3) 
+
+
+def combineTeam(name, season=current_year):
     df = getData(name, 'gamelog', season)
+    sleep(1)
     df.columns = [x.upper() for x in df.columns]
-    sleep(2)
     pid = df['PLAYER_ID'].unique()[0]
     df['TEAM_ID'] = playerInfo['TEAM_ID'].loc[playerInfo['PERSON_ID'] == pid].item()
     total = df['GAME_ID'].count()
@@ -47,7 +83,7 @@ def combineTeam(name, season):
         teamstats = pd.DataFrame(game.Boxscore(str(i), str(season) + '-' + str(int(season)+1)[-2:]).team_stats())
         teamstats.columns = ['TEAM_'+str(x) for x in teamstats.columns]
         ts = ts.append(teamstats)
-        sleep(randint(.1,3))
+        sleep(randint(1,3))
         counter += 1
         print('Finished {0} %'.format(round((counter/total)*100,0)))
     
